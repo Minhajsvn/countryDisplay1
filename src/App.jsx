f59613932b6332d7294c6a402b0b312f432f9ad3
minhajsvn <minhajrafeeq7@gmail.com>
2024-01-29 20:02:27 +0530

    Added the Search input and Search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 export default function App() {
   const [countries, setCountries] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   const fetchCountries = async () => {
     try {
@@ -12,6 +13,38 @@ export default function App() {
         console.log(error);
     }
   };
+
+  const fetchSearchCountries = async (searchText) => {
+    try {
+        const data = await axios.get(`https://restcountries.com/v3.1/name/${searchText}`);
+        setCountries(data.data);
+    } catch (error) {
+      if(error.response.data.status == 404){
+        setCountries([])
+        console.log(error.message);
+      }
+    }
+  };
+
+  const handleChange =(e) => {
+    setSearchText(e.target.value);
+  }
+
+  const inputDiv = {
+    width: "100%",
+    backgroundColor: "#33464c",
+    padding: "20px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  }
+
+  const inputTag = {
+    width: "600px",
+    height:"30px",
+
+  }
 
   const cardStyle = {
     width: "300px",
@@ -36,19 +69,31 @@ export default function App() {
   };
 
   useEffect(() => {
+
     fetchCountries();
   }, []);
+
+  useEffect(() => {
+    fetchSearchCountries(searchText);
+
+  },[searchText, countries]);
+
   return (
-    <div style={divStyle}>
-      {countries.map((country) => (
-        <div key={country.name.common} style={cardStyle}>
-          <img
-            src={country.flags.png}
-            alt={`Flags of ${country.name.common}`}
-          />
-          <h3>{country.name.common}</h3>
-        </div>
-      ))}
+    <div>
+      <div style={inputDiv}>
+        <input style={inputTag} type="text" placeholder="Search for countries" onChange={handleChange} value={searchText}/>
+      </div>
+      <div style={divStyle}>
+        {countries.map((country) => (
+          <div key={country.name.common} style={cardStyle}>
+            <img
+              src={country.flags.png}
+              alt={`Flags of ${country.name.common}`}
+            />
+            <h3>{country.name.common}</h3>
+          </div>
+        ))}
+    </div>
     </div>
   );
 }
